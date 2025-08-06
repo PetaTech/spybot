4f412ae13c6165b72a000410468d59688471dd0f
LBM <official@lovedbymen.com>
2025-08-06 13:27:03 -0500

    change tradier token

--- config/live.py
+++ config/live.py
@@ -8,8 +8,8 @@ from .strategy import *
 
 # === API Configuration ===
 TRADIER_API_URL = 'https://api.tradier.com/v1'
-TRADIER_ACCESS_TOKEN = 'gHHlnkRTiQEcyMIW2qGDIGZnKQMM'
-ACCOUNT_ID = '6YB57635'
+TRADIER_ACCESS_TOKEN = 'bcuTwm0ocPZja7J2L159sBNnGHBT'
+ACCOUNT_ID = '6YB57979'
 
 # === Logging ===
 LOG_LEVEL = 'INFO'
--- config/paper.py
+++ config/paper.py
@@ -8,9 +8,9 @@ from .strategy import *
 
 # === API Configuration ===
 TRADIER_API_URL = 'https://sandbox.tradier.com/v1'
-TRADIER_ACCESS_TOKEN = 'EpBD3Tx9hM9EfBqpTaAVsrJfXjkA'
-ACCOUNT_ID = 'VA60323101'
+TRADIER_ACCESS_TOKEN = 'vZkJpiR828SUs7nO02B0MQ4VmXhd'
+ACCOUNT_ID = 'VA32928735'
 
 # === Logging ===
 LOG_LEVEL = 'INFO'
-LOG_DIR = 'logs' 
+LOG_DIR = 'logs'
